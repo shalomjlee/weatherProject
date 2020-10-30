e8bf95a084f816ac659478209347292212b242ac
shalomjlee <shalomjlee@gmail.com>
2020-10-30 14:16:36 -0500

    working MVP

--- src/App.js
+++ src/App.js
@@ -3,18 +3,37 @@ import React, { useState } from 'react';
 import WeatherInfo from './Components/WeatherInfo';
 import Header from './Components/Header';
 import { UserContext } from './UserContext';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Documentation from './Components/Documentation';
 
 function App() {
 	const [weatherLocation, setWeatherLocation] = useState('');
 	const [searchString, setSearchString] = useState('');
 	const [selectUnit, setSelectUnit] = useState('I');
+	const [tempUnit, setTempUnit] =useState('');
 
+	const getTempUnit = () => {
+		let displayUnit = '';
+		switch (selectUnit) {
+			case 'M':
+				displayUnit = 'C';
+				break;
+			case 'S':
+				displayUnit = 'K';
+				break;
+			default:
+				displayUnit = 'F';
+		}
+
+		return setTempUnit(displayUnit);
+	};
 	const getSearchData = () => {
 		const url = `https://api.weatherbit.io/v2.0/current?city=${searchString}&units=${selectUnit}&key=
 		${process.env.REACT_APP_STOCK_API_KEY}`;
 		fetch(url)
 			.then((res) => res.json())
 			.then((res) => {
+				// setNewSearch(true);
 				console.log(res);
 				return res;
 			})
@@ -34,6 +53,10 @@ function App() {
 					getSearchData,
 					selectUnit,
 					setSelectUnit,
+				
+					tempUnit,
+					setTempUnit,
+					getTempUnit,
 				}}>
 				<Header />
 				<WeatherInfo />
