13860a0d870b0e502f0578e490bf9d429a2f8e2b
shalomjlee <shalomjlee@gmail.com>
2020-10-30 08:39:22 -0500

    fixed api and output

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,36 +1,46 @@
 import React, { useState, useContext } from 'react';
-import { UserContext } from '../UserContext'
-import './Input.css'
+import { UserContext } from '../UserContext';
+import './Input.css';
 const Input = () => {
-    const { searchString, setSearchString, getSearchData} =useContext(UserContext)
-    // const [location, setLocation] = useState('');
-    // console.log({location})
-    return (
-			<div>
-				<h3>Hello from input</h3>
-				<form
-					onSubmit={(event) => {
-						event.preventDefault();
-						getSearchData();
-					}}>
-					<input
-						type='text'
-						placeholder='What location?'
-						value={searchString}
-						onChange={(event) => setSearchString(event.target.value)}
-					/>
-					<button type='submit'>Submit</button>
-				</form>
+	const { searchString, setSearchString, getSearchData, selectUnit, getSelectUnit } = useContext(
+		UserContext
+	);
+	
+	return (
+		<div>
+			<h3>Hello from input</h3>
+			<form
+				onSubmit={(event) => {
+					event.preventDefault();
+					getSearchData();
+				}}>
+				<input
+					type='text'
+					placeholder='What location?'
+					value={searchString}
+					onChange={(event) => setSearchString(event.target.value)}
+				/>
+				<button type='submit'>Submit</button>
+			</form>
+			<form
+				onSubmit={(event) => {
+					event.preventDefault();
+					selectUnit();
+				}}>
 				<select>
-					<option value='F'>Fahrenheit</option>
-					<option value='C'>Celsius</option>
-					<option selected value='K'>
-						Kelvin
+					<option selected value='I'>
+						Fahrenheit
 					</option>
+					<option value='M'>Celsius</option>
+					<option value='S'>Kelvin</option>
 				</select>
-			</div>
-		);
+			</form>
+		</div>
+	);
 };
 
 export default Input;
 //only send API request when button is submitted
+
+//when select a value, fetchs API to change the units={this} portion to the option value
+//i need to figure out how to get select value and set it as the getselectunit.
